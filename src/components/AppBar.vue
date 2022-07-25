a4705f7981f534982ede7cca7eb6264f9e837778
mario <mario.deng@belstar.com.tw>
2022-07-25 18:14:41 +0800

    update icon

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="flex w-full items-center break-inside  text-title h-14 ">
-    <img src="@/assets/img//belstar.svg" class="h-16 -m-3" />
+    <img src="@/assets/img/SSMOLab-RGB.png" style="width: 160px" />
     <h1 class="text-xl ml-3">同源國際商業銀行</h1>
     <section class="text-sm ml-auto flex items-center">
       <h3><span class="text-xl">信用卡</span>消費明細對帳單</h3>
